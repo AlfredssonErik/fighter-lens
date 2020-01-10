6bcb8021b3ec6fb13ac5dd1ae88462677ede51b3
Erik Alfredsson <erik.alfredsson.jb@gmail.com>
2020-01-10 13:50:02 +0100

    Add division selector

--- src/components/UFC/index.js
+++ src/components/UFC/index.js
@@ -1,14 +1,31 @@
 import React, { Component, Fragment } from 'react';
 import './UFC.css';
 
+const Divisions = (props) => {
+	return (
+		<Fragment>
+			<h2 className="modal__title">Pick a division</h2>
+			<div className="picker">
+				{props.divisions && props.divisions.map(item => 
+					<div key={item.name} className="picker__item">
+						<div className="picker__content" onClick={props.handleDivisionPick.bind(null, item.name)}>
+							<span>{item.name}</span>
+						</div>
+					</div>
+				)}
+			</div>
+		</Fragment>
+	);
+};
+
 const Fighters = (props) => {
 	return (
 		<Fragment>
 			<h2 className="modal__title">Pick a fighter</h2>
 			<div className="picker">
 				{props.fighters && props.fighters.map(item => 
-					<div key={item.id} className="picker__item">
-						<div className="picker__content" onClick={props.handleFighterPick.bind(null, item.id)}>
+					<div key={item.name} className="picker__item">
+						<div className="picker__content" onClick={props.handleFighterPick.bind(null, item.link)}>
 							<span>{item.name}</span>
 						</div>
 					</div>
@@ -21,24 +38,40 @@ const Fighters = (props) => {
 class UFC extends Component {
 	constructor(props) {
 		super(props);
-		this.fighters = [
-			{id: 'jon-jones',name: 'Jon Jones'},
-			{id: 'daniel-cormier',name: 'Daniel Cormier'},
-			{id: 'alexander-gustafsson',name: 'Alexander Gustafsson'},
-			{id: 'thiago-santos',name: 'Thiago Santos'},
-			{id: 'anthony-smith',name: 'Anthony Smith'},
-			{id: 'dominick-reyes',name: 'Dominick Reyes'},
-			{id: 'corey-anderson',name: 'Corey Andersson'},
-			{id: 'jan-blachowicz',name: 'Jan Blachowicz'},
-			{id: 'volkan-oezdemir',name: 'Volkan Oezdemir'},
-			{id: 'glover-teixeira',name: 'Glover Teixeira'}
-		];
+		this.state = {
+			divisions: undefined,
+			selectedDivision: undefined
+		};
+		this.handleDivisionPick = this.handleDivisionPick.bind(this);
 		this.handleFighterPick = this.handleFighterPick.bind(this);
 	}
 
-	handleFighterPick(id) {
+	componentDidMount() {
+		fetch('/rankings')
+		.then((response) => {
+			if(!response.ok) throw Error(response.statusText)
+			return response.json();
+		})
+		.then((result) => {
+			this.setState({
+				divisions: result
+			})
+		})
+		.catch(err => console.log(err))
+	}
+
+	handleDivisionPick(division) {
+		const index = this.state.divisions.findIndex(function(item, i){
+			return item.name === division
+		  });
+		this.setState({
+			selectedDivision: index
+		});
+	}
+
+	handleFighterPick(link) {
 		let fighterData;
-		fetch(`/fighter/${id}`)
+		fetch(link)
 		.then((response) => {
 			if(!response.ok) throw Error(response.statusText)
 			return response.json();
@@ -46,15 +79,26 @@ class UFC extends Component {
 		.then((result) => {
 			fighterData = result;
 			fighterData.position = this.props.activePosition;
+			this.setState({
+				selectedDivision: undefined
+			});
 			this.props.onSave(fighterData);
 		})
 		.catch(err => console.log(err))
 	}
 
 	render() {
+		const { selectedDivision } = this.state;
+		let selector;
+		if (selectedDivision === undefined) {
+			selector = <Divisions handleDivisionPick={this.handleDivisionPick} divisions={this.state.divisions} />
+		} else {
+			const fighters = this.state.divisions[this.state.selectedDivision].fighters;
+			selector = <Fighters handleFighterPick={this.handleFighterPick} fighters={fighters} />;
+		}
 		return (
 			<div className="">
-				<Fighters handleFighterPick={this.handleFighterPick} fighters={this.fighters} />
+				{selector}			
 			</div>
 		);
 	}
