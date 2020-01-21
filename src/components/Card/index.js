5e69e596452dd6e99bbc15c9efdbd6ce8939cd3b
Erik Alfredsson <erik.alfredsson.jb@gmail.com>
2020-01-21 19:41:25 +0100

    Add styling and functionality for card flip

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,23 @@
 import React, { Component, Fragment } from 'react';
 import './Card.css';
 
+// https://www.w3schools.com/howto/howto_css_flip_card.asp
 
 export default class Card extends Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			isFlipped: false
+		};
+
+		this.handleFlip = this.handleFlip.bind(this);
 		this.onSelectFighter = this.onSelectFighter.bind(this);
+	}
+
+	handleFlip(e) {
+		e.preventDefault();
+		this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
 	}
 
 	onSelectFighter() {
@@ -16,48 +28,57 @@ export default class Card extends Component {
 		if (this.props.fighter === null || this.props.fighter === undefined) {
 			return (
 				<div className="card card--empty" onClick={this.onSelectFighter}>
-					<span className="card__add">Pick</span>
+					<div className="card__inner">
+						<span className="card__add">Pick</span>
+					</div>
 				</div>
 			);
 		} else {
 			return (
 				<Fragment>
 					<img src={this.props.fighter.image} alt={this.props.fighter.name} className={`fighter__image fighter__image--${this.props.position}`}></img>
-					<div className="card">
-						<button className="card__edit" onClick={this.onSelectFighter}>Switch</button>
-						<h2 className="card__name">{this.props.fighter.name}</h2>
-						<p className="card__total">{this.props.fighter.total}</p>
-						<div className="card__record">
-							{
-								this.props.fighter.record.map((item, index) => 
-								<div key={index}>
-									<span className="card__record-stat">{item.figure}</span><br />
-									<span>{item.text}</span>
+					<div className={`card ${this.state.isFlipped ? 'card--fliped' : ''}`}>
+						<div className="card__inner">
+							<div className="card__front">
+								<button className="card__flip" onClick={this.handleFlip}>View history</button>
+								<button className="card__edit" onClick={this.onSelectFighter}>Switch</button>
+								<h2 className="card__name">{this.props.fighter.name}</h2>
+								<p className="card__total">{this.props.fighter.total}</p>
+								<div className="card__record">
+									{
+										this.props.fighter.record.map((item, index) => 
+										<div key={index}>
+											<span className="card__record-stat">{item.figure}</span><br />
+											<span>{item.text}</span>
+										</div>
+										)
+									}
 								</div>
-								)
-							}
-						</div>
-						<div className="card__stats">
-						{
-							this.props.fighter.stats.map((item, index) => 
-							<div key={index}>
-								<span className="card__accuracy">{item.figure}</span><br />
-								<span>{item.text}</span>
+								<div className="card__stats">
+								{
+									this.props.fighter.stats.map((item, index) => 
+									<div key={index}>
+										<span className="card__accuracy">{item.figure}</span><br />
+										<span>{item.text}</span>
+									</div>
+									)
+								}
+								</div>
+								<div className="card__bio">
+									{
+										this.props.fighter.bio.map((item, index) => 
+										<div key={index} className="card__bio-item">
+											<p className="card__bio-header">{item.text}</p>
+											<span>{item.figure}</span>
+										</div>
+										)
+									}
+								</div>
 							</div>
-							)
-						}
+							<div className="card__back">
+								<button className="card__flip" onClick={this.handleFlip}>View stats</button>
+							</div>
 						</div>
-						<div className="card__bio">
-							{
-								this.props.fighter.bio.map((item, index) => 
-								<div key={index} className="card__bio-item">
-									<p className="card__bio-header">{item.text}</p>
-									<span>{item.figure}</span>
-								</div>
-								)
-							}
-						</div>
-
 					</div>
 				</Fragment>
 			);
